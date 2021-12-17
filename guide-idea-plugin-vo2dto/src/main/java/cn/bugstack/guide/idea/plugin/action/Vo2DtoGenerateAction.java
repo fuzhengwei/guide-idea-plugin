d57d8d664bd3b6b802c7f4f161b9f6a2bcf5db6f
fuzhengwei <184172133@qq.com>
2021-12-17 20:59:03 +0800

    小傅哥，feat：配置打包版本信息

--- guide-idea-plugin-vo2dto/src/main/java/cn/bugstack/guide/idea/plugin/action/Vo2DtoGenerateAction.java
+++ guide-idea-plugin-vo2dto/src/main/java/cn/bugstack/guide/idea/plugin/action/Vo2DtoGenerateAction.java
@@ -2,31 +2,11 @@ package cn.bugstack.guide.idea.plugin.action;
 
 import cn.bugstack.guide.idea.plugin.application.IGenerateVo2Dto;
 import cn.bugstack.guide.idea.plugin.domain.service.impl.GenerateVo2DtoImpl;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.Result;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.ScrollType;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.ui.TestInputDialog;
-import com.intellij.psi.*;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.search.PsiShortNamesCache;
 import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 public class Vo2DtoGenerateAction extends AnAction {
 
